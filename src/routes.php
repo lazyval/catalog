1d7ba5dc8bcbc0f7429c3daa42b76ed7f5b6f065
Kostya Golikov <johnysilver7@gmail.com>
2017-11-14 04:39:43 +0300

    Move memcached and db setup to deps

--- src/routes.php
+++ src/routes.php
@@ -3,55 +3,6 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$container = $app->getContainer();
-
-$container['database'] = function () use ($app) {
-  $db_url = $_ENV['DATABASE_URL'];
-  $db_user = $_ENV['DATABASE_USER'];
-  $db_pwd = $_ENV['DATABASE_PWD'];
-
-  $pdo = new \Slim\PDO\Database(getenv('DATABASE_URL'), getenv('DATABASE_USER'), getenv('DATABASE_PWD'));
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-  return $pdo;
-};
-
-$container['memcached'] = function () use ($app) {
-  // create a new persistent client
-  $m = new Memcached("memcached_pool");
-  $m->setOption(Memcached::OPT_BINARY_PROTOCOL, TRUE);
-
-  // some nicer default options
-  $m->setOption(Memcached::OPT_NO_BLOCK, TRUE);
-  $m->setOption(Memcached::OPT_AUTO_EJECT_HOSTS, TRUE);
-  $m->setOption(Memcached::OPT_CONNECT_TIMEOUT, 2000);
-  $m->setOption(Memcached::OPT_POLL_TIMEOUT, 2000);
-  $m->setOption(Memcached::OPT_RETRY_TIMEOUT, 2);
-
-  // setup authentication
-  $username = getenv("MEMCACHEDCLOUD_USERNAME");
-  $password = getenv("MEMCACHEDCLOUD_PASSWORD");
-  if (!empty($username) && !empty($password)) {
-    $m->setSaslAuthData($username, $password);
-  }
-
-  // We use a consistent connection to memcached, so only add in the
-  // servers first time through otherwise we end up duplicating our
-  // connections to the server.
-  if (!$m->getServerList()) {
-      // parse server config
-      $servers = explode(",", getenv("MEMCACHEDCLOUD_SERVERS"));
-      foreach ($servers as $s) {
-          $parts = explode(":", $s);
-          $m->addServer($parts[0], $parts[1]);
-      }
-  }
-
-  return $m;
-};
-
-
 $app->get('/', function (Request $request, Response $response, array $args) {
   return $this->renderer->render($response, 'index.phtml', $args);
 });
